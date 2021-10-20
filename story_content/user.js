b48f10e450f726b3e272b9463650d345706332d9
DimasEka15 <dimasekaid@gmail.com>
2021-10-20 14:45:17 +0800

    first-commit

---
+++ story_content/user.js
@@ -0,0 +1,126 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5qNf30BI7fB":
+        Script1();
+        break;
+      case "64StIERs6yd":
+        Script2();
+        break;
+      case "6jgHn03BXzI":
+        Script3();
+        break;
+      case "6OpzzjrO2kQ":
+        Script4();
+        break;
+      case "5zPMdTPPcs8":
+        Script5();
+        break;
+      case "5fkOIjcUOrR":
+        Script6();
+        break;
+      case "6PlZ3c19DHI":
+        Script7();
+        break;
+      case "5w5p9ajdbdi":
+        Script8();
+        break;
+      case "6jqq8obQEQF":
+        Script9();
+        break;
+      case "61YPbJewexW":
+        Script10();
+        break;
+      case "6BHRYDqEsE1":
+        Script11();
+        break;
+      case "6WodTnPiiTr":
+        Script12();
+        break;
+      case "5vyVpO3UWMV":
+        Script13();
+        break;
+  }
+}
+
+function Script1()
+{
+  const audio = document.getElementById("bgSong");
+audio.src="song/backsound.mp3";
+audio.load();
+audio.play();
+}
+
+function Script2()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 1
+}
+
+function Script3()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 0
+}
+
+function Script4()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 1
+}
+
+function Script5()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 0
+}
+
+function Script6()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 1
+}
+
+function Script7()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 0
+}
+
+function Script8()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 1
+}
+
+function Script9()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 0
+}
+
+function Script10()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 1
+}
+
+function Script11()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 0
+}
+
+function Script12()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 1
+}
+
+function Script13()
+{
+  const audio = document.getElementById("bgSong");
+audio.volume = 0
+}
+
