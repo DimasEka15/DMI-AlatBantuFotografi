34e82203f7fa6c3e4682537c5526197b628d900e
DimasEka15 <dimasekaid@gmail.com>
2021-10-23 14:21:17 +0800

    update sfx quiz

--- story_content/user.js
+++ story_content/user.js
@@ -2,43 +2,43 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5qNf30BI7fB":
+      case "5eIwQTJ5tEi":
         Script1();
         break;
-      case "64StIERs6yd":
+      case "5rFHbQORSfs":
         Script2();
         break;
-      case "6jgHn03BXzI":
+      case "5abb7GWeqWf":
         Script3();
         break;
-      case "6OpzzjrO2kQ":
+      case "5j9FWmb1OwK":
         Script4();
         break;
-      case "5zPMdTPPcs8":
+      case "6QZy8yBm3Dv":
         Script5();
         break;
-      case "5fkOIjcUOrR":
+      case "5wo7mp5X9Mc":
         Script6();
         break;
-      case "6PlZ3c19DHI":
+      case "6Z2RTezQVAU":
         Script7();
         break;
-      case "5w5p9ajdbdi":
+      case "6jdPZeuJ6te":
         Script8();
         break;
-      case "6jqq8obQEQF":
+      case "6lVg0KH26M3":
         Script9();
         break;
-      case "61YPbJewexW":
+      case "6W3EZ3eygqo":
         Script10();
         break;
-      case "6BHRYDqEsE1":
+      case "69ngyE3eChF":
         Script11();
         break;
-      case "6WodTnPiiTr":
+      case "5uq0ciw2edC":
         Script12();
         break;
-      case "5vyVpO3UWMV":
+      case "6FMzobIqTNe":
         Script13();
         break;
   }
@@ -55,72 +55,72 @@ audio.play();
 function Script2()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 1
+audio.play()
 }
 
 function Script3()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 0
+audio.pause()
 }
 
 function Script4()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 1
+audio.play()
 }
 
 function Script5()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 0
+audio.pause()
 }
 
 function Script6()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 1
+audio.play()
 }
 
 function Script7()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 0
+audio.pause()
 }
 
 function Script8()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 1
+audio.play()
 }
 
 function Script9()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 0
+audio.pause()
 }
 
 function Script10()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 1
+audio.play()
 }
 
 function Script11()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 0
+audio.pause()
 }
 
 function Script12()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 1
+audio.play()
 }
 
 function Script13()
 {
   const audio = document.getElementById("bgSong");
-audio.volume = 0
+audio.pause()
 }
 
